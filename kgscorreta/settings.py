6f0009ac8bb49b641f7d5b4a1e4da3ab661831d3
Guigdsdev <guigdsdev@gmail.com>
2025-05-07 15:16:55 -0300

    update2

--- kgscorreta/settings.py
+++ kgscorreta/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = 'kgscorreta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nomedobanco',
+        'USER': 'nomedousuario',
+        'PASSWORD': 'suasenhaforte',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
